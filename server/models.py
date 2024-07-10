72efc0bb248e912909d04602109f057ae66f07f3
Rayngumi <rayngumi25@gmail.com>
2024-07-10 11:59:05 +0300

    updated relationships

--- server/models.py
+++ server/models.py
@@ -13,7 +13,7 @@ class Owner(db.Model):
     address = db.Column(db.String, nullable=False)
 
     # Relationship with Vehicle (one-to-many)
-    vehicles = db.relationship("Vehicle", backref='owners') 
+    vehicles = db.relationship("Vehicle", backref='owner', lazy='dynamic') 
 
     def __repr__(self):
         return f"Owner(id={self.owner_id}, name='{self.owner_name}', age={self.age})"
@@ -29,10 +29,10 @@ class Feature(db.Model):
     car = db.Column(db.String, nullable=False)
     
     # Relationship with Vehicle (many-to-many)
-    vehicles = db.relationship("Vehicle", secondary="vehicle_features")  
+    vehicles = db.relationship("Vehicle", secondary="vehicle_features", back_populates="features")
 
     def __repr__(self):
-        return f"<Feature(id={self.id}, name={self.name}, car_cc={self.car_cc}, fuel_type={self.fuel_type})>"
+        return f"<Feature(id={self.feature_id}, name={self.name}, car_cc={self.car_cc}, fuel_type={self.fuel_type})>"
 
 
 class Vehicle(db.Model):
@@ -49,13 +49,13 @@ class Vehicle(db.Model):
 
     # Relationship with Owner (one-to-many)
     owner_id = db.Column(db.Integer, db.ForeignKey('owners.owner_id'))
-    owner = db.relationship("Owner")  
+    owner = db.relationship("Owner", back_populates="vehicles")
 
     # Relationship with Feature (many-to-many)
-    features = db.relationship("Feature", secondary="vehicle_features") 
+    features = db.relationship("Feature", secondary="vehicle_features", back_populates="vehicles")
 
     def __repr__(self):
-        return f'<Vehicle {self.id} {self.make} {self.model}>'
+        return f'<Vehicle(id={self.vehicle_id}, make={self.make}, model={self.model})>'
 
 
 class Rental(db.Model):
@@ -65,23 +65,20 @@ class Rental(db.Model):
     vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.vehicle_id')) 
     duration_days = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)  
+    
+    # Relationship with Vehicle (one-to-one)
+    vehicle = db.relationship("Vehicle", backref=db.backref('rental', uselist=False))
 
     def __repr__(self):
         return f"Rental ID: {self.rental_id} - Vehicle: {self.vehicle_id} - Duration: {self.duration_days} days"
+
 
 # Association table (many-to-many relationship between Vehicle and Feature)
 class VehicleFeatures(db.Model):
     __tablename__ = 'vehicle_features'
 
-    vehicle_id = db.Column('vehicle_id', db.Integer, db.ForeignKey('vehicles.vehicle_id'), primary_key=True)
-    feature_id = db.Column('feature_id', db.Integer, db.ForeignKey('features.feature_id'), primary_key=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.vehicle_id'), primary_key=True)
+    feature_id = db.Column(db.Integer, db.ForeignKey('features.feature_id'), primary_key=True)
 
     def __repr__(self):
         return f"<VehicleFeatures(vehicle_id={self.vehicle_id}, feature_id={self.feature_id})>"
-
-
-
-
-
-
-
